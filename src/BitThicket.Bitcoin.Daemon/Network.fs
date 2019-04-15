a7a61e52aadf0afd4ca6832c669ca979f782ffe0
Ben Collins <aggieben@gmail.com>
2019-04-15 07:29:03 -0500

    [wip] thinking over how to manage peer agents/jobs

--- src/BitThicket.Bitcoin.Daemon/Network.fs
+++ src/BitThicket.Bitcoin.Daemon/Network.fs
@@ -1,15 +1,25 @@
 module BitThicket.Bitcoin.Daemon.Network
-
-let dnsSeeds = 
-    [| "dnsseed.bluematt.me";
-       "seed.bitcoin.sipa.be";
-       "dnsseed.bitcoin.dashjr.org";
-       "seed.bitcoinstats.com";
-       "seed.bitcoin.jonasschnelli.ch";
-       "seed.btc.petertodd.org" |]
+open System.Collections.Generic
 
 (*
 testnet seeds:
 testnet-seed.bitcoin.jonasschnelli.ch
 seed.tbtc.petertodd.org
 *)
+
+module private PeerLookup =
+    let dnsSeeds = 
+       [| "dnsseed.bluematt.me";
+       "seed.bitcoin.sipa.be";
+       "dnsseed.bitcoin.dashjr.org";
+       "seed.bitcoinstats.com";
+       "seed.bitcoin.jonasschnelli.ch";
+       "seed.btc.petertodd.org" |]
+
+    type PeerDescriptor =
+       { address : string }
+       
+    let peers = HashSet<PeerDescriptor>()
+
+    
+
